45cb872c04507fd3403be6f6a0df1185b19f94de
tearne <tearne@gmail.com>
2019-03-08 07:15:12 +0000

    playing with dependent types

--- scala/02_modelling/src/main/scala/ODEModelRunner.scala
+++ scala/02_modelling/src/main/scala/ODEModelRunner.scala
@@ -4,24 +4,31 @@ import org.apache.commons.math3.ode.sampling.{FixedStepHandler, StepNormalizer}
 
 import scala.collection.mutable
 
-trait ODE[P] extends FirstOrderDifferentialEquations {
-  val params: P
+trait ODE extends FirstOrderDifferentialEquations {
+  type State
+//  trait State
+
+//  val params: P
   val y0: State
 
-  override def getDimension: Int = y0.toArray.length
+  def toArray(s: State): Array[Double]
+  def toState(arr: Array[Double]): State
+//  override def getDimension: Int = y0.toArray.length
 }
 
 trait State {
   def toArray: Array[Double]
 }
 
-trait ODEModel[P, ODEState] {
-  val timeSpan: Range
-  val ode: ODE[P]
+object ODESolver {
+//  val timeSpan: Range
+//  val ode: ODE
 
-  def stateFromArray(a: Array[Double]): ODEState
+//  def stateFromArray(a: Array[Double]): ODE.State
 
-  def solve(): Map[Int, ODEState] = {
+  def test(ode: ODE): ode.State = ???
+
+  def solve[C <: ODE](ode: C, timeSpan: Range): Map[Int, ode.State] = {
     val relTol = 1e-11
     val absTol = 1e-11
     val minStep = 1e-8
@@ -29,13 +36,13 @@ trait ODEModel[P, ODEState] {
     val timeZero = timeSpan.min
     val numDays = timeSpan.length + 1
 
-    val steps = mutable.Buffer[ODEState]()
+    val steps = mutable.Buffer[ode.State]()
 
     val stepHandler: FixedStepHandler = new FixedStepHandler {
       override def init(t0: Double, y0: Array[Double], t: Double): Unit = {}
 
       override def handleStep(t: Double, y: Array[Double], yDot: Array[Double], isLast: Boolean): Unit = {
-        steps.append(stateFromArray(y))
+        steps.append(ode.toState(y))
       }
     }
 
@@ -44,15 +51,21 @@ trait ODEModel[P, ODEState] {
     val out = Array.fill(ode.getDimension)(0.0)
 
     dp853.addStepHandler(stepNormalizer)
-    dp853.integrate(ode, timeZero, ode.y0.toArray, numDays, out)
+    dp853.integrate(ode, timeZero, ode.toArray(ode.y0), numDays, out)
 
-    val integrated: List[ODEState] = steps.toList
+    val integrated: List[ode.State] = steps.toList
 
-    timeSpan.foldLeft(Map[Int, ODEState]()) { case (map, t) => map.updated(t, integrated(t)) }
+    timeSpan.foldLeft(Map[Int, ode.State]()) { case (map, t) => map.updated(t, integrated(t)) }
   }
 
-  def solveAndPrint(): Unit = {
-    val sortedTimeSeries: Seq[(Int, ODEState)] = solve().toSeq.sortBy(_._1)
+  def thing(ode: ODE): Unit ={
+    val t: ode.State = test(ode)
+  }
+
+  def solveAndPrint[C <: ODE](ode: C, range: Range): Unit = {
+//    type T = ode.State
+//    val t: ode.State = test(ode)
+    val sortedTimeSeries: Seq[(Int, C#State)] = solve(ode, range).toSeq.sortBy(_._1)
     for (state <- sortedTimeSeries) {
       println(s"t = ${state._1}, ${state._2}")
     }
--- scala/02_modelling/src/main/scala/demoODEModelRunner.scala
+++ scala/02_modelling/src/main/scala/demoODEModelRunner.scala
@@ -1,26 +1,25 @@
-object demoODEModelRunner extends App {
-
-  object MyODEModel extends ODEModel[Parameters, ODEState] {
-    override val timeSpan: Range = 0 to 10
-    override val ode: ODE[Parameters] = new ODE[Parameters] {
-      override val params: Parameters = Parameters(2.0)
-      override val y0: State = ODEState(10, 20)
-
-      override def computeDerivatives(t: Double, y: Array[Double], yDot: Array[Double]): Unit = {
-        import params._
-        yDot(0) = a * y(1)
-        yDot(1) = -a * y(0)
-      }
-    }
-
-    override def stateFromArray(a: Array[Double]): ODEState = ODEState(a(0), a(1))
-  }
-
-  case class Parameters(a: Double)
-
-  case class ODEState(s: Double, i: Double) extends State {
-    override def toArray: Array[Double] = Array(s, i)
-  }
-
-  MyODEModel.solveAndPrint()
-}
+//object demoODEModelRunner extends App {
+//
+//  object MyODEModel extends ODEModel[Parameters, ODEState] {
+//    override val timeSpan: Range = 0 to 10
+//    override val ode: ODE[Parameters] = new ODE[Parameters] {
+//      val param = 2.0
+//      val y0: State = ODEState(10, 20)
+//
+//      override def computeDerivatives(t: Double, y: Array[Double], yDot: Array[Double]): Unit = {
+//        yDot(0) = param * y(1)
+//        yDot(1) = -param * y(0)
+//      }
+//    }
+//
+//    override def stateFromArray(a: Array[Double]): ODEState = ODEState(a(0), a(1))
+//  }
+//
+//  case class Parameters(a: Double)
+//
+//  case class ODEState(s: Double, i: Double) extends State {
+//    override def toArray: Array[Double] = Array(s, i)
+//  }
+//
+//  MyODEModel.solveAndPrint()
+//}
