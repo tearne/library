919e4f6d6b96947a58d87291cda8f52f1b924ee6
tearne <tearne@gmail.com>
2019-03-29 09:49:17 +0000

    Add comment about universal packager

--- scala/02_modelling/src/main/scala/LoadResource.scala
+++ scala/02_modelling/src/main/scala/LoadResource.scala
@@ -8,7 +8,8 @@ import scala.io.Source
 //  2) On the command line with jars and configs.json in a lib dir: java -cp "lib/*:lib" LoadResource
 //  where lib contains jars and resources
 //
-//  To build far jar just "clean" and "package" and jar will appear in target
+//  To build far jar just "clean" and "package" and jar will appear in target/scala-2.xx
+//  Alternatively, if using the native packager, run "stage" and look in target/universal/stage
 
 object LoadResource extends App {
   val resourceName = "my.resource"
