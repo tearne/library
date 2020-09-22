248fdf1e45df72024d1ce8b3ea6ccef18412aa5a
tearne <tearne@gmail.com>
2020-09-22 22:12:31 +0100

    scanning dot

--- rust/03_ubercorn/src/main.rs
+++ rust/03_ubercorn/src/main.rs
@@ -1,46 +1,66 @@
 use spidev::{SpiModeFlags, Spidev, SpidevOptions};
 use rgb;
 use std::io::prelude::*;
+use std::{thread, time};
 
-struct Cleanup;
+const BLACK: rgb::RGB8 = rgb::RGB8 { r: 0, g: 0, b: 0 };
 
-impl Drop for Cleanup {
+struct Display {
+    spi: Spidev,
+}
+
+impl Display {
+    pub fn build() -> Self {
+        let mut spi = Spidev::open("/dev/spidev0.0").unwrap();
+        let options = SpidevOptions::new()
+            .bits_per_word(8)
+            .max_speed_hz(9_000_000)
+            .mode(SpiModeFlags::SPI_MODE_0)
+            .build();
+        spi.configure(&options);
+
+        Display{spi}
+    }
+
+    pub fn apply(&mut self, leds: &[rgb::RGB8]){
+        self.spi.write(&[0x72]);
+        let data = Self::as_array(&leds);
+        self.spi.write(&data);
+    }
+
+    fn as_array(leds: &[rgb::RGB8]) -> Vec<u8> {
+        let mut arr: Vec<u8> = vec![];
+        use rgb::ComponentSlice;
+        arr.extend_from_slice(leds.as_slice());
+        arr
+    }
+}
+
+impl Drop for Display {
     fn drop(&mut self) {
-        eprintln!("Doing some final cleanup");
+        let leds: [rgb::RGB8; 256] =  [BLACK; 256];
+        self.apply(&leds);
     }
 }
 
 pub fn main() {
-    let _cleanup = Cleanup;
-
-    const BLACK: rgb::RGB8 = rgb::RGB8 { r: 0, g: 0, b: 0 };
+    let mut display = Display::build();
 
     let mut leds: [rgb::RGB8; 256] =  [BLACK; 256];
-
-    let mut spi = Spidev::open("/dev/spidev0.0").unwrap();
-    let options = SpidevOptions::new()
-        .bits_per_word(8)
-        .max_speed_hz(9_000_000)
-        .mode(SpiModeFlags::SPI_MODE_0)
-        .build();
-    spi.configure(&options);
 
     let y = 3;
     let x = 7;
     leds[(y * 16) + x] = rgb::RGB8::new(0, 255, 0);
     
-    plot_stuff(&mut spi, &leds);
-}
+    display.apply(&leds);
 
-fn plot_stuff(spi: &mut Spidev, leds: &[rgb::RGB8]){
-    spi.write(&[0x72]);
-    let data = as_array(&leds);
-    spi.write(&data);
-}
-
-fn as_array(leds: &[rgb::RGB8]) -> Vec<u8> {
-    let mut arr: Vec<u8> = vec![];
-    use rgb::ComponentSlice;
-    arr.extend_from_slice(leds.as_slice());
-    arr
+    let mut counter = 0;
+    let delay = time::Duration::from_millis(50);
+    loop {
+        leds[counter] = BLACK;
+	counter = (counter + 1) % 255;
+	leds[counter] = rgb::RGB8::new(0, 255, 0);
+        display.apply(&leds);
+        thread::sleep(delay);
+    }
 }
