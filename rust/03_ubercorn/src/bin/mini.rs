d3742cb6e76f9cd0f43d1e623859008d877913a0
tearne <tearne@gmail.com>
2021-08-26 21:13:37 +0000

    Small progress with tokio & buttons, wondering if Ubuntu 20.04 is to blame for X & Y not working

--- rust/03_ubercorn/src/bin/mini.rs
+++ rust/03_ubercorn/src/bin/mini.rs
@@ -1,7 +1,9 @@
-use gpio_cdev::{AsyncLineEventHandle, Chip, EventRequestFlags, Line, LineHandle, LineRequestFlags, Lines};
+use futures::{FutureExt};
+use gpio_cdev::{Chip, EventRequestFlags, Line, LineHandle, LineRequestFlags, Lines};
 // use rppal::gpio::{Gpio, InputPin, OutputPin, Trigger};
 use spidev::{SpiModeFlags, Spidev, SpidevOptions};
-use std::{io::Write, ops::Range};
+use tokio::{sync::mpsc::{Receiver, channel}, task::JoinHandle};
+use std::{borrow::Cow, io::Write, ops::Range};
 
 //TODO
 // Use: https://crates.io/crates/gpio-cdev
@@ -32,8 +34,7 @@ const CMD_SCROLL_CTRL:[u8;2] = [0x20, 0x00];
 const BUTTON_A: u32 = 5;
 const BUTTON_B: u32 = 6;
 const BUTTON_X: u32 = 16;
-const BUTTON_Y: u32 = 20;
-const BUTTONS: [u32;4] = [BUTTON_A, BUTTON_B, BUTTON_X, BUTTON_Y]; 
+const BUTTON_Y: u32 = 24;
 
 const LUT: [[usize; 3]; 119] = [[139, 138, 137], [223, 222, 221], [167, 166, 165], [195, 194, 193], [111, 110, 109], [55, 54, 53], [83, 82, 81], [136, 135, 134], [220, 219, 218], [164, 163, 162], [192, 191, 190], [108, 107, 106], [52, 51, 50], [80, 79, 78], [113, 115, 114], [197, 199, 198], [141, 143, 142], [169, 171, 170], [85, 87, 86], [29, 31, 30], [57, 59, 58], [116, 118, 117], [200, 202, 201], [144, 146, 145], [172, 174, 173], [88, 90, 89], [32, 34, 33], [60, 62, 61], [119, 121, 120], [203, 205, 204], [147, 149, 148], [175, 177, 176], [91, 93, 92], [35, 37, 36], [63, 65, 64], [122, 124, 123], [206, 208, 207], [150, 152, 151], [178, 180, 179], [94, 96, 95], [38, 40, 39], [66, 68, 67], [125, 127, 126], [209, 211, 210], [153, 155, 154], [181, 183, 182], [97, 99, 98], [41, 43, 42], [69, 71, 70], [128, 130, 129], [212, 214, 213], [156, 158, 157], [184, 186, 185], [100, 102, 101], [44, 46, 45], [72, 74, 73], [131, 133, 132], [215, 217, 216], [159, 161, 160], [187, 189, 188], [103, 105, 104], [47, 49, 48], [75, 77, 76], [363, 362, 361], [447, 446, 445], [391, 390, 389], [419, 418, 417], [335, 334, 333], [279, 278, 277], [307, 306, 305], [360, 359, 358], [444, 443, 442], [388, 387, 386], [416, 415, 414], [332, 331, 330], [276, 275, 274], [304, 303, 302], [337, 339, 338], [421, 423, 422], [365, 367, 366], [393, 395, 394], [309, 311, 310], [253, 255, 254], [281, 283, 282], [340, 342, 341], [424, 426, 425], [368, 370, 369], [396, 398, 397], [312, 314, 313], [256, 258, 257], [284, 286, 285], [343, 345, 344], [427, 429, 428], [371, 373, 372], [399, 401, 400], [315, 317, 316], [259, 261, 260], [287, 289, 288], [346, 348, 347], [430, 432, 431], [374, 376, 375], [402, 404, 403], [318, 320, 319], [262, 264, 263], [290, 292, 291], [349, 351, 350], [433, 435, 434], [377, 379, 378], [405, 407, 406], [321, 323, 322], [265, 267, 266], [293, 295, 294], [352, 354, 353], [436, 438, 437], [380, 382, 381], [408, 410, 409], [324, 326, 325], [268, 270, 269], [296, 298, 297]];
 const BUF_SIZE: usize = 28 * 8;
@@ -159,18 +160,145 @@ async fn main() {
     um.flush();
 
 
-    let mut events = um.buttons[0].async_events(
+    //https://docs.rs/gpio-cdev/0.4.0/gpio_cdev/struct.Line.html#method.events
+    //https://stackoverflow.com/questions/62376239/rust-tokio-infinite-loop-for-multiple-buttons-listening
+    //https://tokio.rs/tokio/tutorial/select
+
+    // fn button_receiver(button: &Line, consumer_name: Cow<'static, str>) -> Receiver<()> {
+    //     let (sender, receiver) = channel::<()>(16);
+    
+    //     let events = button.events(
+    //         LineRequestFlags::INPUT, 
+    //         EventRequestFlags::BOTH_EDGES,
+    //         &consumer_name)
+    //         .unwrap();
+        
+    //     tokio::task::spawn_blocking(move || {
+    //         for _ in events {
+    //             sender.try_send(()).expect(&format!("panic on {}", consumer_name));
+    //         }
+    //     });
+
+    //     receiver
+    // }
+
+    // let mut a = button_receiver(&um.buttons[0], "button_a_listener".into());
+    // let mut b = button_receiver(&um.buttons[1], "button_b_listener".into());
+    // let mut x = button_receiver(&um.buttons[2], "button_x_listener".into());
+    // let mut y = button_receiver(&um.buttons[3], "button_y_listener".into());
+
+    // tokio::task::spawn(async move {
+    //     loop {
+    //         tokio::select! {
+    //             Some(_) = a.recv() => {
+    //                 println!("Button A")
+    //             }
+    //             Some(_) = b.recv() => {
+    //                 println!("Button B");
+    //             }
+    //             Some(_) = x.recv() => {
+    //                 println!("Button X")
+    //             }
+    //             Some(_) = y.recv() => {
+    //                 println!("Button Y");
+    //             }
+    //             else => break
+    //         };
+    //     }
+    // });
+
+    let (b_A_sender, mut b_A_receiver) = channel(16);
+    let (b_B_sender, mut b_B_receiver) = channel(16);
+    let (b_X_sender, mut b_X_receiver) = channel(16);
+    let (b_Y_sender, mut b_Y_receiver) = channel(16);
+
+    let events_A = um.buttons[0].events(
         LineRequestFlags::INPUT, 
         EventRequestFlags::BOTH_EDGES,
-        "button-a-events").unwrap();
+        "um-but-a")
+        .unwrap();
+    let b_A_blocking_task = tokio::task::spawn_blocking(move || {
+        for _ in events_A {
+            println!("a");
+            b_A_sender.try_send(()).unwrap();
+        }
+    });
+
+    let events_B = um.buttons[1].events(
+        LineRequestFlags::INPUT, 
+        EventRequestFlags::BOTH_EDGES,
+        "um-but-b")
+        .unwrap();
+    let b_B_blocking_task = tokio::task::spawn_blocking(move || {
+        for _ in events_B {
+            println!("b");
+            b_B_sender.try_send(()).unwrap();
+        }
+    });
+
+    let events_X = um.buttons[2].events(
+        LineRequestFlags::INPUT, 
+        EventRequestFlags::BOTH_EDGES,
+        "um-but-x")
+        .unwrap();
+    let b_X_blocking_task = tokio::task::spawn_blocking(move || {
+        for _ in events_X {
+            println!("x"); 
+            b_X_sender.try_send(()).unwrap();
+        }
+    });
+
+    let events_Y = um.buttons[3].events(
+        LineRequestFlags::INPUT, 
+        EventRequestFlags::BOTH_EDGES,
+        "um-but-y")
+        .unwrap();
+    let b_Y_blocking_task = tokio::task::spawn_blocking(move || {
+        for _ in events_Y {
+            println!("y");
+            b_Y_sender.try_send(()).unwrap();
+        }
+    });
+
+    let (_,_,_,_,_) = tokio::join!(
+        b_A_blocking_task,
+        b_B_blocking_task,
+        b_X_blocking_task,
+        b_Y_blocking_task,
+        tokio::task::spawn(async move {
+            loop {
+                tokio::select! {
+                    Some(_) = b_A_receiver.recv() => {
+                        println!("Button A")
+                    }
+                    Some(_) = b_B_receiver.recv() => {
+                        println!("Button B");
+                    }
+                    Some(_) = b_X_receiver.recv() => {
+                        println!("Button X")
+                    }
+                    Some(_) = b_Y_receiver.recv() => {
+                        println!("Button Y");
+                    }
+                    else => break
+                };
+            }
+        })
+    );
+
+
+    use futures::stream::StreamExt;
+    
+
+    
 
     // let events = t.await;
 
-    use futures::stream::StreamExt;
-    while let Some(event) = events.next().await {
-        let event = event.unwrap();
-        println!("GPIO Event: {:?}", event);
-    }
+    
+    // while let Some(event) = events.next().await {
+    //     let event = event.unwrap();
+    //     println!("GPIO Event: {:?}", event);
+    // }
 
     // for event in um.buttons[0].events(
     //     LineRequestFlags::INPUT,
