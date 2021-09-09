d5eca0b9629e8fddc60f1e8d761059fd52b300a2
tearne <tearne@gmail.com>
2021-09-09 14:34:04 +0000

    TRY wIndows Native GUI

---
+++ rust/14_windows/src/main.rs
@@ -0,0 +1,56 @@
+// #![windows_subsystem = "windows"]
+/*!
+    A very simple application that shows your name in a message box.
+    Unlike `basic_d`, this example uses layout to position the controls in the window
+*/
+
+use std::path::Path;
+
+use native_windows_gui as nwg;
+use native_windows_derive as nwd;
+
+use self::nwd::NwgUi;
+use self::nwg::NativeUi;
+
+#[derive(Default, NwgUi)]
+pub struct BasicApp {
+    #[nwg_control(size: (300, 115), position: (300, 300), title: "Basic example", flags: "WINDOW|VISIBLE")]
+    #[nwg_events( OnWindowClose: [BasicApp::say_goodbye] )]
+    window: nwg::Window,
+
+    #[nwg_layout(parent: window, spacing: 1)]
+    grid: nwg::GridLayout,
+
+    #[nwg_control(text: "Heisenberg", focus: true)]
+    #[nwg_layout_item(layout: grid, row: 0, col: 0)]
+    name_edit: nwg::TextInput,
+
+    #[nwg_control(text: "Say my name")]
+    #[nwg_layout_item(layout: grid, col: 0, row: 1, row_span: 2)]
+    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
+    hello_button: nwg::Button
+}
+
+impl BasicApp {
+    fn say_hello(&self) {
+        nwg::modal_info_message(&self.window, "Hello", &format!("Hello {}", self.name_edit.text()));
+    }
+    
+    fn say_goodbye(&self) {
+        nwg::modal_info_message(&self.window, "Goodbye", &format!("Goodbye {}", self.name_edit.text()));
+        nwg::stop_thread_dispatch();
+    }
+
+}
+
+fn main() {
+    println!("abcd");
+
+    let p = std::env::current_dir().unwrap();
+    println!("--==--> {:?}", p);
+
+    nwg::init().expect("Failed to init Native Windows GUI");
+    nwg::Font::set_global_family("Segoe UI").expect("Failed to set default font");
+    let _app = BasicApp::build_ui(Default::default()).expect("Failed to build UI");
+    nwg::dispatch_thread_events();
+}
