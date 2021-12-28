ccf7a676581dc762d672c9daf08882d87bedfaff
tearne <tearne@gmail.com>
2021-12-28 19:45:37 +0000

    Runs

--- rust/15_game_of_life/src/main.rs
+++ rust/15_game_of_life/src/main.rs
@@ -1,54 +1,101 @@
-use std::time::Duration;
+mod coords;
 
-use rgb::{RGB, RGB8};
+use std::{time::Duration, collections::VecDeque};
+
+use once_cell::sync::Lazy;
+use rand::Rng;
+use rgb::RGB8;
 use unicorn::pimoroni::unicorn::Unicorn;
 
-const OFFSETS: [(u8,u8); 8] = [
-    (0,14),
-    (0,15),
-    (15,14),
-    (15,0),
-    (15,15),
-    (14,14),
-    (14,0),
-    (14,15)
-];
+static OFFSETS: Lazy<Vec<(u8, u8)>> = Lazy::new(|| {
+    vec![
+        (-1,-1),
+        (0,-1),
+        (1,-1),
+        (-1,0),
+        (1,0),
+        (-1,1),
+        (0,1),
+        (1,1)
+    ].iter().map(|(x,y)|((x+16) as u8, (y+16) as u8))
+    .collect::<Vec<(u8,u8)>>()
+});
 
-const BLACK: RGB8 = RGB8::new(0,0,0);
-const RED: RGB8 = RGB8::new(100,0,0);
+static BLACK: RGB8 = RGB8::new(0,0,0);
+static GREEN: RGB8 = RGB8::new(0, 100, 0);
+static RED: RGB8 = RGB8::new(50,0,0);
 
-struct Grid(Vec<RGB8>);
-impl Grid {
-    pub fn new() -> Self {
-        Grid(vec![RGB::new(255, 0, 0); 256])
+struct Layer {
+    colour: RGB8,
+    grids: VecDeque<Grid>,
+}
+impl Layer {
+    pub fn new(colour: RGB8) -> Self {
+        let mut grids = VecDeque::<Grid>::new();
+        grids.push_front(Grid::random());
+
+        Layer {
+            colour,
+            grids,
+        }
     }
 
-    pub fn num_live_neighbours_at(&self, i:u8, j:u8) -> u8 {
-        let prev_colour_at = |i:u8, j:u8| -> RGB8 {
-            self.0[(i + j * 16) as usize] 
+    pub fn render(&self, display: &mut Unicorn) {
+        for (idx, is_alive) in self.grids.front().unwrap().0.iter().enumerate() {
+            display.set_idx(idx, if *is_alive { &self.colour } else { &BLACK } )
+        }
+    }
+
+    pub fn has_repition(&self) -> bool {
+        let current = self.grids.front().unwrap();
+        self.grids.iter().skip(1).any(|x| x == current)
+    }
+
+    pub fn evolve(&mut self) {
+        let next = self.grids.front().unwrap().evolve();
+        self.grids.push_front(next);
+        self.grids.truncate(3);
+    }
+}
+
+
+#[derive(Default, PartialEq)]
+struct Grid(Vec<bool>);
+impl Grid {
+    pub fn random() -> Self {
+        let mut rng = rand::thread_rng();
+        let vec: Vec<_> = (0..256).map(|_|{ rng.gen::<bool>() }).collect();
+        Grid(vec)
+    }
+
+    pub fn num_live_neighbours_at(&self, xy:(u8,u8)) -> u8 {
+        let is_alive_at = |xy:(u8,u8)| -> bool {
+            self.0[coords::to_idx(xy)] 
         };
 
-        OFFSETS.iter().map(|(a,b)|{
-            if prev_colour_at((i+a)%16, (j+b)%16) != BLACK { Some(true) }
-            else { None }
-        }).count() as u8
+        let count = OFFSETS.iter().map(|(a,b)|{
+            let offset_xy = ((xy.0 + a)%16, (xy.1 + b)%16);
+            is_alive_at(offset_xy)
+        }).fold(0,|acc,next_is_alive|{
+            if !next_is_alive {acc}
+            else {acc+1}
+        }) as u8;
+
+        count
     }
 
     pub fn evolve(&self) -> Self {
-        let new_grid: Vec<RGB8> = (0..self.0.len())
+        let new_grid: Vec<bool> = (0..self.0.len())
             .map(|i: usize|{
-                let x = (i % 16) as u8;
-                let y = (i / 16) as u8; 
-
-                let px = self.0[i];
-                let nbr_count = self.num_live_neighbours_at(x,y);
-                if px == BLACK {
-                    if nbr_count == 3 { RED }
-                    else { BLACK }
-                } else {
-                    if nbr_count < 2 { BLACK }
-                    else if nbr_count > 3 { BLACK }
-                    else { RED }
+                let is_alive = self.0[i];
+                let nbr_count = self.num_live_neighbours_at(coords::to_xy(i));
+                if !is_alive {
+                    // Dead cell comes to life if 3 live neighbours
+                    nbr_count == 3 
+                } 
+                else { 
+                    // Live cell dies if < 2 or > 3 live neighbours
+                    (2..=3).contains(&nbr_count) 
                 }
             })
             .collect();
@@ -57,46 +104,20 @@ impl Grid {
     }
 }
 
-struct World {
-    pub grid: [RGB8; 256],
-    pub previous: [RGB8; 256],
-}
-impl World {
-    pub fn new() -> Self {
-        World{
-            grid: [RGB::new(255, 0, 0); 256],
-            previous: [RGB::new(255, 0, 0); 256],
-        }
-    }
-
-    pub fn evolve(&mut self) -> Self {
-        World{
-            grid: self.grid.evolve(),
-            previous: g,
-        }
-    }
-}
-
 
 fn main() {
-    println!("Hello, world!");
-
     let mut display = Unicorn::new();
-    let r = RGB::new(255, 0, 0);
-    let g = RGB::new(0, 255, 0);
-    let b = RGB::new(0, 0, 255);
+    
+    loop {
+        let mut layer = Layer::new(GREEN);
+        loop {
+            layer.render(&mut display);
+            display.flush();
 
-    display.set_xy(0, 0, &r);
-    display.set_xy(1, 0, &r);
-    display.set_xy(2, 0, &r);
+            std::thread::sleep(Duration::from_millis(100));
 
-    display.set_xy(3, 0, &r);
-    display.set_xy(4, 0, &g);
-    display.set_xy(4, 1, &b);
-
-    display.set_xy(4, 2, &b);
-    display.set_xy(4, 3, &b);
-    display.set_xy(4, 4, &b);
-    display.flush();
-    std::thread::sleep(Duration::from_millis(10000));
+            if layer.has_repition() { break; }
+            else { layer.evolve() }
+        }
+    }
 }
