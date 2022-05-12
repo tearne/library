4cd5b554b6c16f24242c582d9d99cef407f6c42b
Tearne <oliver.tearne@apha.gov.uk>
2022-05-12 19:23:04 +0100

    Experimenting

--- python/09_mongodb/query.py
+++ python/09_mongodb/query.py
@@ -1,11 +1,22 @@
 import pymongo
+import argparse
+from pprint import pprint
 
 parser = argparse.ArgumentParser(description='Read stuffs from MongoDB.')
 parser.add_argument('-d', dest="database", help='database')
 parser.add_argument('-c', dest="collection", help='collection')
+args = parser.parse_args()
 
-client = MongoClient("mongodb://localhost:27017")
+client = pymongo.MongoClient("mongodb://localhost:27017")
 
-db=client.otdb
-result=db.testing.find()
-pprint(result)
+db=client[args.database]
+pprint(db)
+pprint("--------")
+
+collection = db[args.collection]
+pprint(collection)
+pprint("--------")
+
+result=collection.find({})
+
+pprint(result[1]["meta"])
